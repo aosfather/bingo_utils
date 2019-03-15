589e81c41405eb3ce897ab80ac28005bb6093d26
xiongxiaopeng <xiongxiaopeng@ehomepay.com.cn>
2019-03-15 19:43:42 +0800

    add md5 and httpspost method

---
+++ crypto.go
@@ -0,0 +1,32 @@
+package bingo_utils
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"sort"
+	"strings"
+)
+
+func ToMd5Hex(str string) string {
+	h := md5.New()
+	h.Write([]byte(str)) // 需要加密的字符串
+	cipherStr := h.Sum(nil)
+	return hex.EncodeToString(cipherStr) // 输出加密结果
+}
+
+/**
+  连接变量名和变量值，并按变量名进行排序
+*/
+func JoinAndSort(p map[string]string, contact, split string) string {
+	var s, keys []string
+	for k, _ := range p {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		s = append(s, k+contact+p[k])
+	}
+
+	return strings.Join(s, split)
+}
--- http.go
+++ http.go
@@ -1,13 +1,97 @@
 package bingo_utils
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/http/cookiejar"
 	"net/url"
 	"strings"
 )
+
+func HttpsPost(theUrl string, data interface{}, result interface{}) error {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	//http cookie接口
+	cookieJar, _ := cookiejar.New(nil)
+
+	c := &http.Client{
+		Jar:       cookieJar,
+		Transport: tr,
+	}
+
+	content, _ := json.Marshal(data)
+	fmt.Println(string(content))
+	resp, err := c.Post(theUrl, "application/json;charset=utf-8", strings.NewReader(string(content)))
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(body))
+
+	json.Unmarshal(body, result)
+	return nil
+}
+
+func HttpsPostForm(theUrl string, params map[string]string, result interface{}) error {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	//http cookie接口
+	cookieJar, _ := cookiejar.New(nil)
+
+	c := &http.Client{
+		Jar:       cookieJar,
+		Transport: tr,
+	}
+
+	var values url.Values = make(map[string][]string)
+	for key, val := range params {
+		values.Set(key, val)
+	}
+
+	resp, err := c.PostForm(theUrl, values)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(string(body))
+
+	json.Unmarshal(body, result)
+	return nil
+}
+
+//https get
+func HttpsGet(client *http.Client, url string) ([]byte, error) {
+
+	response, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("http get error : uri=%v , statusCode=%v", url, response.StatusCode)
+	}
+	return ioutil.ReadAll(response.Body)
+}
 
 //HTTPGet get 请求
 func HTTPGet(uri string) ([]byte, error) {
@@ -35,6 +119,8 @@ func Post(theUrl string, data interface{}, result interface{}) error {
 	if err != nil {
 		return err
 	}
+
+	fmt.Println(string(body))
 
 	json.Unmarshal(body, result)
 	return nil
