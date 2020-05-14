f8599dbd145d43b86e3f0f84f837743eff6e6aa9
xiongxiaopeng <xiongxiaopeng@ehomepay.com.cn>
2020-05-14 18:57:11 +0800

    增加luascript实现

---
+++ lua/luascript.go
@@ -0,0 +1,148 @@
+package lua
+
+import (
+	"fmt"
+	lua "github.com/yuin/gopher-lua"
+	"log"
+)
+
+var _default_option = NewLuaOption()
+
+type LuaLogFunction func(string)
+type LuaContext map[string]interface{}
+type LuaScript struct {
+	pool     *LuaPool
+	function *lua.FunctionProto
+	Context  LuaContext
+	Log      LuaLogFunction
+}
+
+//设置lua vm pool
+func (this *LuaScript) SetPool(p *LuaPool) {
+	this.pool = p
+}
+
+//加载脚本文件
+func (this *LuaScript) Loadfile(filename string) {
+	var err error
+	this.function, err = CompileByfile(filename)
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
+
+//加载脚本
+func (this *LuaScript) Load(name, content string) {
+	var err error
+	this.function, err = CompileByString(name, content)
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
+
+func (this *LuaScript) Call() (interface{}, error) {
+	var l *lua.LState
+	if this.pool != nil {
+		l = this.pool.Get()
+		defer func() {
+			this.pool.Put(l)
+		}()
+	}
+	if l == nil {
+		return nil, fmt.Errorf("no lua vm!")
+	}
+	lfunc := l.NewFunctionFromProto(this.function)
+	l.Push(lfunc)
+	this.initFunctions(l)
+	err := l.PCall(0, 1, l.NewFunction(this.errHandle))
+	return ToGoValue(l.Get(-1), _default_option), err
+}
+
+func (this *LuaScript) CallByParame(funcname string, p ...interface{}) (interface{}, error) {
+	var l *lua.LState
+	if this.pool != nil {
+		l = this.pool.Get()
+		defer func() {
+			this.pool.Put(l)
+		}()
+	}
+	if l == nil {
+		return nil, fmt.Errorf("no lua vm!")
+	}
+
+	//初始化vm
+	lfunc := l.NewFunctionFromProto(this.function)
+	l.Push(lfunc)
+	this.initFunctions(l)
+	err := l.PCall(0, 1, l.NewFunction(this.errHandle))
+	//获取函数名称
+	fn := l.GetGlobal(funcname)
+	var paramters []lua.LValue
+	for _, pitem := range p {
+		paramters = append(paramters, ToLuaValue(pitem))
+	}
+
+	err = l.CallByParam(lua.P{
+		Fn:      fn,
+		NRet:    1,
+		Protect: true,
+	}, paramters...)
+
+	return ToGoValue(l.Get(-1), _default_option), err
+}
+
+//错误捕获器
+func (this *LuaScript) errHandle(l *lua.LState) int {
+	log.Println(l.Get(-1).String())
+	return 1
+}
+
+func (this *LuaScript) luaLog(l *lua.LState) int {
+	content := l.Get(-1).String()
+	l.Pop(1)
+	if this.Log != nil {
+		this.Log(content)
+	} else {
+		log.Println("lualog:" + content)
+	}
+
+	return 1
+}
+
+//从context中获取参数
+func (this *LuaScript) luaGetContext(l *lua.LState) int {
+	key := l.Get(-1).String()
+	l.Pop(1)
+
+	var value interface{}
+	if this.Context != nil {
+		value = this.Context[key]
+	}
+	//压入结果
+	l.Push(ToLuaValue(value))
+	return 1
+}
+
+//从context中获取参数
+func (this *LuaScript) luaSetContext(l *lua.LState) int {
+	value := l.Get(-1)
+	l.Pop(1)
+
+	key := l.Get(-1).String()
+	l.Pop(1)
+
+	if this.Context != nil {
+		this.Context[key] = ToGoValue(value, _default_option)
+	}
+
+	return 1
+}
+
+func (this *LuaScript) initFunctions(l *lua.LState) {
+	if l == nil {
+		return
+	}
+	l.SetGlobal("getContext", l.NewFunction(this.luaGetContext))
+	l.SetGlobal("setContext", l.NewFunction(this.luaSetContext))
+	l.SetGlobal("log", l.NewFunction(this.luaLog))
+}
---
+++ lua/luascript_test.go
@@ -0,0 +1,54 @@
+package lua
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestLuaScript_Load(t *testing.T) {
+	script := LuaScript{}
+	script.pool = NewLuaPool(10, nil)
+	s := `
+
+function name()
+    return 'dishonesty'
+end
+
+
+function incCount(n)
+    log("haha")
+    print(n)
+ 	a=2+1
+ 	print(a)
+ 	person = {
+	  	name = "Michel",
+	  	age  = "31", -- weakly input
+	  	work_place = "San Jose"
+    	}
+ 	return person
+end
+return "hello"
+`
+	script.Load("test", s)
+	logfunc := func(a string) {
+		t.Log(a)
+	}
+	script.Log = logfunc
+	for i := 0; i < 100; i++ {
+		go func() {
+			v, err := script.CallByParame("incCount", fmt.Sprintf("hi%d", i))
+			if err != nil {
+				t.Log(err.Error())
+				return
+			}
+			t.Log(script.Call())
+
+			t.Log(v.(map[interface{}]interface{}))
+			t.Log(v)
+		}()
+		time.Sleep(10 * time.Microsecond)
+	}
+	time.Sleep(10 * time.Second)
+
+}
