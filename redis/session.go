4e2a7299ccc69a2c765b07e6f5ed8ec1251f14ec
xiongxiaopeng <xiongxiaopeng@ehomepay.com.cn>
2020-05-27 18:18:41 +0800

    增加redis session store的实现

--- redis/session.go
+++ redis/session.go
@@ -1,31 +1,46 @@
 package redis
 
-import "github.com/go-redis/redis"
+import (
+	"github.com/go-redis/redis"
+	"strings"
+)
 
 //分布式session存储实现
 type RedisSessionStore struct {
-	Addr   string //地址
-	Db     int    //数据库
-	Pwd    string //密码
 	client *redis.Client
-	Expire int64 //超期时间
+	expire int64 //超期时间
 }
 
-func (this *RedisSessionStore) Init() {
+func (this *RedisSessionStore) Init(addr string, db int, pwd string, expire int64) {
 	//防止多次初始化
 	if this.client != nil {
 		return
 	}
 
-	if this.Db < 0 || this.Db > 16 {
-		this.Db = 0
+	if expire > 0 {
+		this.expire = expire
+	}
+	if addr == "" {
+		return
 	}
 
-	this.client = redis.NewClient(&redis.Options{
-		Addr:     this.Addr,
-		Password: this.Pwd, // no password set
-		DB:       this.Db,  // use default DB
-	})
+	if db < 0 || db > 16 {
+		db = 0
+	}
+	var option redis.Options
+	//if strings.TrimSpace(pwd)=="" {
+	//	option=redis.Options{
+	//		Addr:     addr,
+	//		DB:       db,  // use default DB
+	//	}
+	//}else {
+	option = redis.Options{
+		Addr:     addr,
+		Password: strings.TrimSpace(pwd), // no password set
+		DB:       db,                     // use default DB
+	}
+	//}
+	this.client = redis.NewClient(&option)
 }
 
 func (this *RedisSessionStore) Exist(id string) bool {
@@ -41,8 +56,8 @@ func (this *RedisSessionStore) Exist(id string) bool {
 
 func (this *RedisSessionStore) Create(id string) {
 	this.client.HSetNX(id, "_id_", id)
-	if this.Expire > 0 {
-		this.client.Expire(id, ToSecond(this.Expire))
+	if this.expire > 0 {
+		this.client.Expire(id, ToSecond(this.expire))
 	}
 
 }
@@ -50,7 +65,11 @@ func (this *RedisSessionStore) GetValue(id, key string) interface{} {
 	if id != "" && key != "" {
 		result := this.client.HGet(id, key)
 		if result != nil {
-			return result.String()
+			v, err := result.Result()
+			if err != nil {
+				return nil
+			}
+			return v
 		}
 	}
 	return nil
@@ -61,8 +80,8 @@ func (this *RedisSessionStore) SetValue(id, key string, value interface{}) {
 	}
 }
 func (this *RedisSessionStore) Touch(id string) {
-	if this.Expire > 0 {
-		this.client.Expire(id, ToSecond(this.Expire))
+	if this.expire > 0 {
+		this.client.Expire(id, ToSecond(this.expire))
 	}
 }
 
